9b88dfd70859193816ebc3f35bf02789148495d0
koreaitclass <koreaitclass@gmail.com>
2023-08-11 15:41:48 +0900

    upload_0.03 게시물 열람/삭제 기능 구현중

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -10,9 +10,13 @@ public class Main {
     Scanner scan = new Scanner(System.in);
     int lastId = 0;
 
-    List<Article> articles = new ArrayList<>(); // 생성
+    List<Article> articles = new ArrayList<>(); // 배열로 생성
 
-
+//  게시글 열람 및 삭제 관련
+//  split()
+//  startsWith()
+//  "arraylist 요소" 삭제 검색
+//  "자바 현재 날짜"
 
 
     while (true) {
@@ -29,6 +33,8 @@ public class Main {
       else if (cmd.equals("help")) {
         System.out.println("article list - 게시글 목록");
         System.out.println("article write - 게시글 작성");
+        System.out.println("article view (int) - 게시글 열람");
+        System.out.println("article delete (int) - 게시글 삭제");
         System.out.println("system exit - 프로그램 종료");
       }
 
@@ -54,7 +60,7 @@ public class Main {
         System.out.printf("내용 : ");
         String content = scan.nextLine();
 
-        Article article = new Article(id, title, content);`
+        Article article = new Article(id, title, content);
         articles.add(article);
 
         System.out.printf("%d번 게시글이 생성되었습니다.\n", lastId);
@@ -72,6 +78,11 @@ public class Main {
           System.out.printf("제목 : %s\n내용 : %s\n", article.title, article.content);
         }
       }
+
+      else if (cmd.equals("article delete")) {
+
+      }
+
 
       else
         System.out.println("존재하지 않는 명령어입니다.");
